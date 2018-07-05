9a84d0c8cfacb21693c609b9b316c0a051d83d18
momskyh <37992953+momskyh@users.noreply.github.com>
2018-07-05 21:47:38 +0700

    Update botackv2.php

--- botackv2.php
+++ botackv2.php
@@ -18,7 +18,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
 }else if($arrJson['events'][0]['message']['text'] == "ขอชื่อ"){
   $arrPostData = array();
-  $userId = $arrJson['events'][0]['source']['userId']
+  $userId = $arrJson['events'][0]['source']['userId'];
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$userId;
