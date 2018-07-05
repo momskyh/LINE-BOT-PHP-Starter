3a1a50ace606536ebc9dddf874495c943f845c5d
momskyh <37992953+momskyh@users.noreply.github.com>
2018-07-05 21:45:01 +0700

    Update botackv2.php

--- botackv2.php
+++ botackv2.php
@@ -16,6 +16,12 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "ขอชื่อ"){
+  $arrPostData = array();
+  $userId = $arrJson['events'][0]['source']['userId']
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$userId;
 }else if($arrJson['events'][0]['message']['text'] == "ขอกลุ่ม"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
