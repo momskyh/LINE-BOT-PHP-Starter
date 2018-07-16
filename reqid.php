9702dbae2d491ce154db0dcf1fe8b4f1edae4655
momskyh <37992953+momskyh@users.noreply.github.com>
2018-07-16 14:08:32 +0700

    Update reqid.php

--- reqid.php
+++ reqid.php
@@ -15,7 +15,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+  $arrPostData['messages'][0]['text'] = "สวัสดี ข้อความ คุณคือ ".$arrJson['events'][0]['source']['messages'];
  
 }else{
   $arrPostData = array();
